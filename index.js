76a8cae7a83d8f11a2dd4070cc314bb5cfa83a6b
Kamal <obviouslykamal@gmail.com>
2021-03-15 13:57:58 +0530

    clear cache from redis while updating the data

--- index.js
+++ index.js
@@ -14,40 +14,71 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-
-checkCache = (req, res, next) => {
-	const { id } = req.params;
-  	redis_client.get(id, (err, data) => {
-    		if (err) {
-      			console.log(err);
-      			res.status(500).send(err);
-    		}
-    		
-    		if (data != null) {
-      			res.send(data);
-    		} else {
-      			next();
-    		}
-  	});
+checkCache = (id) => {
+	return new Promise((resolve, reject) => {
+		redis_client.get(id, (err, data) => {
+			if(err){
+				reject(err);
+			}
+			if(data!= null){
+				resolve(data);
+			}else{
+				resolve(1);
+			}
+		});
+	});
 };
 
-app.get("/pokemon/:id", checkCache, async (req, res) => {
+clearCache = (id) => {
+	return new Promise((resolve, reject) => {
+		redis_client.del(id, (err, data)=> {
+			resolve(data);
+		});
+	});
+}
+
+
+app.get("/pokemon/:id", async (req, res) => {
 	try {
     		const { id } = req.params;
-    		const pokemonApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    		const pokemon = pokemonApi.data;
-		const response = {};
-		response.name = pokemon.species.name;
-		response.image = pokemon.sprites.other.dream_world.front_default;
-		response.weight = pokemon.weight;
-		response.abilities = pokemon.abilities;
-		redis_client.setex(id, 3600, JSON.stringify(response));
+		let response = await checkCache(id);
+		if(response === 1){
+			const pokemonApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+			const pokemon = pokemonApi.data;
+			const responseData = {}
+			responseData.name = pokemon.species.name;
+			responseData.image = pokemon.sprites.other.dream_world.front_default;
+			responseData.weight = pokemon.weight;
+			responseData.abilities = pokemon.abilities;
+			redis_client.setex(id, 3600, JSON.stringify(responseData));
+			response = responseData;
+		}
     		return res.json(response);
   	}catch (error) {
     		console.log(error);
     		return res.status(500).json(error);
   	}
 });
+
+app.get("/pokemon/edit/:id", async (req, res) => {
+	try{
+		const { id } = req.params;
+                const pokemonApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+                const pokemon = pokemonApi.data;
+                const response = {};
+                response.name = "updated Pokemon";
+                response.image = pokemon.sprites.other.dream_world.front_default;
+                response.weight = "updated Weight";
+                response.abilities = "updated Abilities";
+		await clearCache(id);
+		return res.json(response);
+	}catch(error){
+		console.log(error);
+		return res.status(500).json(error);
+	}
+});
+
+
 
 
 app.listen(port, () => console.log(`Server running on Port ${port}`));
