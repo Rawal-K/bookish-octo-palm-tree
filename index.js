1f602c934a703e9b9c537acec36556a22a7f2e1d
Kamal <obviouslykamal@gmail.com>
2021-03-18 15:33:07 +0530

    Removed root route

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ require('./routes/pokemon.routes.js')(app);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-});
-
 app.listen(port, () => console.log(`Server running on Port ${port}`));
